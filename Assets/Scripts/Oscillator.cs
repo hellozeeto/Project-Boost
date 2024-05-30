fe66c9d5d91775d67ce17d6811d46f077e142933
hellozeeto <tktma1712@gmail.com>
2024-05-30 17:30:00 +0900

    58.진동을 위한 Mathf.Sin()함수 -완-

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField]Vector3 movementVector;
+    float movementFactor; 
+    [SerializeField] float period = 2f;
+
+    void Start()
+    {
+        startingPosition = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period; // 시간에 따라 계속해서 증가함
+
+        const float tau = Mathf.PI * 2; // 6.283으로 일정한 값
+        float rawSinWave = Mathf.Sin(cycles * tau); // -1에서 1로 값이 올라갔다 내려온다.
+
+        movementFactor = (rawSinWave + 1f) / 2; // 1을 더하고 2로 나눠 값을 0~1의 값으로 오르락내리락한다.
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
