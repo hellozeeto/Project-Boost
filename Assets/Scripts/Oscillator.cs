d8ff50455f63cc88f0293f6db059467b3f2335dc
hellozeeto <tktma1712@gmail.com>
2024-05-31 16:10:17 +0900

    63강 -섹션3- 완료

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -18,6 +18,7 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(period <= Mathf.Epsilon) { return; }
         float cycles = Time.time / period; // 시간에 따라 계속해서 증가함
 
         const float tau = Mathf.PI * 2; // 6.283으로 일정한 값
